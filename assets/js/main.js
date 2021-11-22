8de9d5d22647dc539ef576f8336abffc5155d38b
LeventeBack <backistvanlevente17@gmail.com>
2021-11-22 16:01:27 +0200

    added modal close

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,10 @@ const CYCLE_LOOP = [0, 1, 0, 2];
 let keyPresses = {};
 let frameCount = 0; 
 let currentLoopIndex = 0;
+let isMobile = false;
+let tapped;
+let activeVendor;
+let isFullScreen = false;
 
 // HTML SELECTORS
 const canvas = document.getElementById('canvas');
@@ -31,7 +35,9 @@ const ctx = canvas.getContext('2d');
 const players = document.querySelectorAll('[data-gender]');
 const startButton = document.querySelector('[data-button="start"]');
 const gameMenu = document.querySelector('#menu')
-
+const gameWrapper =  document.querySelector('.sponsors-game-wrapper');
+const fullscreenButton = document.querySelector('[data-fullscreen]');
+const modalClose = document.querySelector('[data-close]');
 
 // CLASS INSTANCES
 const player = new Player(ctx);
@@ -42,17 +48,59 @@ export let room = new Room(ctx, 1, 1);
 window.addEventListener('keydown', keyDownHandler);
 window.addEventListener('keyup', keyUpHandler);
 
+
+fullscreenButton.addEventListener('click', () => {
+  if(document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement)
+    closeFullscreen();
+  else
+    openFullscreen(gameWrapper);
+  fullscreenButton.blur()
+})
+
 players.forEach(player => player.addEventListener('click', () => selectPlayer(player)));
 
 startButton.addEventListener('click', startGame)
 
+canvas.addEventListener("touchstart", (e) => {
+  if (!tapped)
+     tapped = setTimeout(() => tapped = null, 300); //wait 300ms then run single click code
+  else {
+    clearTimeout(tapped); //stop single tap callback
+    tapped = null
+    if(activeVendor && !modal.isVisible()){
+      modal.load(activeVendor.company);
+      modal.show();
+    }
+  }
+  e.preventDefault()
+});
+
+canvas.addEventListener('touchmove', touchHandler);
+canvas.addEventListener('touchend', () => keyPresses = {})
+
+modalClose.addEventListener('click', () => modal.hide());
+
 // EVENT HANDLER FUNCTIONS 
 function keyDownHandler(event) {
-    keyPresses[event.key] = true;
+  keyPresses[event.key] = true;
+  if(event.key === "Escape" || event.key === "Space")
+    event.preventDefault();
+
+  if(modal.isVisible())
+    modal.hide();
 }
 
 function keyUpHandler(event) {
     keyPresses[event.key] = false;
+}
+
+function touchHandler(event) {
+  const touchPos = event.touches[0];
+
+  //keyPresses[KEYS[DIRECTIONS.LEFT][0]] = (touchPos.clientX < player.x + player.width/2);
+  // keyPresses[KEYS[DIRECTIONS.RIGHT][0]] = (touchPos.clientX > player.x + player.width/2);
+  // keyPresses[KEYS[DIRECTIONS.UP][0]] = (touchPos.clientY < player.y + player.height/2);
+  // keyPresses[KEYS[DIRECTIONS.DOWN][0]] = (touchPos.clientY > player.y + player.height/2);
 }
 
 function selectPlayer(playerElement){
@@ -76,6 +124,11 @@ function startGame(){
 function animation(){
   const clearOffset = 100
   ctx.clearRect(-clearOffset, -clearOffset, canvas.width + clearOffset, canvas.height + clearOffset);
+
+  ctx.strokeStyle  = 'red';
+  ctx.beginPath();
+  ctx.rect(-1, -1, CANVAS.WIDTH + 2, CANVAS.HEIGHT + 2);
+  ctx.stroke();
 
   // movement check
   let hasMoved = false;
@@ -137,7 +190,7 @@ function animation(){
 }
 
 function vendorAnimation(){
-  let activeVendor;
+  activeVendor = null;
 
   room.vendors.forEach(vendor => {
     if(vendor.collides(player)){
@@ -145,23 +198,27 @@ function vendorAnimation(){
       ctx.textAlign = 'center';
       ctx.fillStyle = 'black';
       ctx.font = "28px Comic Sans MS";
-      ctx.fillText("Press space to check the company's details", canvas.width/2, canvas.height/2 + 10);
+      if(isMobile)
+        ctx.fillText("Double tap to see the company's details", canvas.width/2, canvas.height/2 + 10);
+      else 
+        ctx.fillText("Press space to check the company's details", canvas.width/2, canvas.height/2 + 10);
     } 
-
   });
 
   if(activeVendor){
-    if(keyPresses[" "] && !modal.isVisible()) {
-
-      modal.load(activeVendor.company);
-      modal.show();
-    } 
-    if(keyPresses["Escape"]) {
-      modal.hide();
+    if(keyPresses[" "]){
+      keyPresses[" "] = false;
+      if(!modal.isVisible()) {
+        modal.load(activeVendor.company);
+        modal.show();
+      } 
     }
+    // if(keyPresses["Escape"]) {
+    //   modal.hide();
+    // }
   } 
   else if(modal.isVisible())
-  modal.hide();
+    modal.hide();
 }
 
 function isDirectionPressed(direction) {
@@ -171,22 +228,38 @@ function isDirectionPressed(direction) {
 
 //  SETUP FUNCTIONS
 function setup(){
-  setUpCanvas();
+  canvas.width = CANVAS.WIDTH;
+  canvas.height = CANVAS.HEIGHT;
   PLAYER.SCALE = CANVAS.HEIGHT / 7 / PLAYER.HEIGHT;
   player.setGender('male');
 
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    document.querySelector('.sponsors-game-wrapper').classList.add('mobile');
+    isMobile = true;
+    gameWrapper.classList.add('mobile');
   }
 }
 
-function setUpCanvas(){
-  canvas.width = CANVAS.WIDTH;
-  canvas.height = CANVAS.HEIGHT;
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+  isFullScreen = true;
 }
 
-document.querySelector('[data-fullscreen]').addEventListener('click', () => {
-  canvas.requestFullscreen();
-})
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+  isFullScreen = false;
+}
+
 
 setup();
